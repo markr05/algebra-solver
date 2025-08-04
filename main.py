73001ea876994acad7486a2b405742b3259857e6
Mark Roberts <markrbts2023@gmail.com>
2025-08-04 16:49:07 -0500

    Added a function to do equations with no variables through the use of 2 stacks

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+OPERATIONS = "+-*/="
+OPERATION_PRECEDENCE = {"*": 1, "/": 1, "+": 2, "-": 2}
 def parse_equation(equation):
   equation = [char for char in equation]
   parsed_equation = []
-  operations = "+-*/="
   left = 0
 
   while left < len(equation):
     right = left
-    if equation[left] in operations:
+    if equation[left] in OPERATIONS:
       parsed_equation.append(equation[left])
       left += 1
     else:
@@ -30,29 +31,50 @@ class Algebra:
   def __str__(self):
     return self.equation
   
-  def add(self, index_1, index_2):
-    return int(self.list_of_terms[index_1]) + int(self.list_of_terms[index_2])
+  def add(self, term1, term2):
+    return float(term1) + float(term2)
   
-  def subtract(self, index_1, index_2):
-    return int(self.list_of_terms[index_1]) - int(self.list_of_terms[index_2])
+  def subtract(self, term1, term2):
+    return float(term1) - float(term2)
   
-  def multiply(self, index_1, index_2):
-    return int(self.list_of_terms[index_1]) * int(self.list_of_terms[index_2])
+  def multiply(self, term1, term2):
+    return float(term1) * float(term2)
 
-  def divide(self, index_1, index_2):
-    return int(self.list_of_terms[index_1]) / int(self.list_of_terms[index_2])
+  def divide(self, term1, term2):
+    return float(term1) / float(term2)
   
-  def simple_equation(self, indices):
-    if self.list_of_terms[indices[1]] == "+":
-      return self.add(indices[0], indices[2])
-    if self.list_of_terms[indices[1]] == "-":
-      return self.subtract(indices[0], indices[2])
-    if self.list_of_terms[indices[1]] == "*":
-      return self.multiply(indices[0], indices[2])
-    if self.list_of_terms[indices[1]] == "/":
-      return self.divide(indices[0], indices[2])
+  def simple_equation(self, list):
+    if list[1] == "+":
+      return self.add(list[0], list[2])
+    if list[1] == "-":
+      return self.subtract(list[0], list[2])
+    if list[1] == "*":
+      return self.multiply(list[0], list[2])
+    if list[1] == "/":
+      return self.divide(list[0], list[2])
+    
+  def simple_stack_equation(self):
+    self.operands_stack = []
+    self.operations_stack = []
+    for i in range(len(self.list_of_terms)):
+      if self.list_of_terms[i] in OPERATIONS: 
+        if len(self.operations_stack) >= 1 and OPERATION_PRECEDENCE[self.list_of_terms[i]] >= OPERATION_PRECEDENCE[self.operations_stack[-1]]:
+          equation = self.simple_equation([self.operands_stack.pop(0), self.operations_stack.pop(0), self.operands_stack.pop(0)])
+          self.operands_stack.append(equation)
+        self.operations_stack.append(self.list_of_terms[i])
+      elif self.list_of_terms[i].isnumeric():
+        self.operands_stack.append(self.list_of_terms[i])
+    while len(self.operations_stack) != 0:
+      equation = self.simple_equation([self.operands_stack.pop(0), self.operations_stack.pop(0), self.operands_stack.pop(0)])
+      self.operands_stack.append(equation)
+      
+    
+    
 
 
-my_equation = Algebra("3/3=x")
 
-print(my_equation.simple_equation([0, 1, 2]))
+my_equation = Algebra("3/3*2-4/12*99+400")
+
+my_equation.simple_stack_equation()
+print(f'operations - {my_equation.operations_stack}')
+print(f'terms - {my_equation.operands_stack}')
